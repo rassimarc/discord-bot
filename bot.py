faae752d806f84fce935e609e8022a56266388d6
moukh <marcrassi@outlook.com>
2023-06-05 14:56:47 +0300

    Finished the front-end of the code

--- bot.py
+++ bot.py
@@ -4,11 +4,8 @@ from disnake.ui import View, Button
 from disnake import ui
 from dotenv import load_dotenv
 import os
-import sqlite3
-
 
 load_dotenv()
-
 
 # Intents are required to get the member object when a button is clicked
 intents = disnake.Intents.all()
@@ -32,6 +29,8 @@ async def on_ready():
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def setup(ctx):
+    global category
+    category = await ctx.guild.create_category("League of Legends Games")
     # Check if the command is used in a server
     if ctx.guild is None:
         return
@@ -41,16 +40,27 @@ async def setup(ctx):
         await ctx.send("I don't have the permissions to manage channels.")
         return
 
+
+    embed = disnake.Embed(
+        title="⚔ League of Legends Queue ⚔",
+        description="Click the buttons below to vote for an option.",
+        color=disnake.Color.red()
+    )
+    with open("LOL.jpg", "rb") as file:
+        embed.set_image(url="attachment://lol_image.jpg")
+        file = disnake.File(file, filename="lol_image.jpg")
+        await ctx.send(embed=embed, file=file)    #embed.set_thumbnail(url="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2016_27/1141049/hot-grandpa-001-tease-today-160705.JPG")  # Replace with your bot thumbnail URL
+
     # Create the voting message with buttons
     view = View()
-    view.add_item(Button( label="TOP", custom_id="TOP"))
-    view.add_item(Button( label="MID", custom_id="MID"))
-    view.add_item(Button( label="SUP", custom_id="SUP"))
-    view.add_item(Button( label="ADC", custom_id="ADC"))
-    view.add_item(Button( label="JUNGLE", custom_id="JUNGLE"))
+    view.add_item(Button(style=disnake.ButtonStyle.green, label="Top", custom_id="TOP"))
+    view.add_item(Button(style=disnake.ButtonStyle.green, label="Mid", custom_id="MID"))
+    view.add_item(Button(style=disnake.ButtonStyle.green, label="Support", custom_id="SUP"))
+    view.add_item(Button(style=disnake.ButtonStyle.green, label="ADC", custom_id="ADC"))
+    view.add_item(Button(style=disnake.ButtonStyle.green, label="Jungle", custom_id="JUNGLE"))
     view.add_item(Button(style = disnake.ButtonStyle.red , label = "Leave" , custom_id = "leave"))
 
-    voting_message = await ctx.send("Vote for an option:", view=view)
+    voting_message = await ctx.send(view=view)
 
     # Store the message ID for later reference
     global voting_message_id
@@ -58,16 +68,31 @@ async def setup(ctx):
 
 @bot.event
 async def on_button_click(inter):
-    
+    #await inter.response.defer()  # Acknowledge the interaction
+
     if inter.message.id == voting_message_id:
         guild = inter.guild
         member = guild.get_member(inter.user.id)
-        channel = guild.get_channel(1114156750598328413)
+        channel = guild.get_channel(inter.channel_id)
 
         # Check if the user is in the guild and the channel exists
         if member and channel:
             # Check if the button label corresponds to an option
             option = inter.component.custom_id
+            if option != 'leave':
+                embed = disnake.Embed(
+                    title="Queue Joined",
+                    description=f"✅ You have joined the queue as a {option}.",
+                    color=disnake.Color.green()
+                )
+                await inter.response.send_message(embed=embed, ephemeral=True, delete_after=10)
+            else:
+                embed = disnake.Embed(
+                    title="Queue Left",
+                    description="❌ You have left the queue.",
+                    color=disnake.Color.red()
+                )
+                await inter.response.send_message(embed=embed, ephemeral=True, delete_after=10)
             if option == 'leave':
                 for options, queue in voting_options.items():
                     if member in queue:
@@ -80,9 +105,11 @@ async def on_button_click(inter):
                 voting_options[option].append(member)
 
                 # Check if each option has been chosen at least twice
-                if len(voting_options['TOP']) >= 2:
+                #if len(voting_options['TOP']) >= 1:
+                if all(len(queue) >= 2 for queue in voting_options.values()):
                     new_channel = await guild.create_text_channel(
-                        name="Game1",
+                        name="Game",
+                        category=category,
                         overwrites={
                             guild.default_role: disnake.PermissionOverwrite(read_messages=False),
                         }
@@ -93,6 +120,6 @@ async def on_button_click(inter):
                             if queue:
                                 member = queue.pop(0)
                                 await new_channel.set_permissions(member, read_messages=True)
-                                await new_channel.send(f"Welcome {member.mention} to the Game1 channel!")
+                                await new_channel.send(f"Welcome {member.mention} to the channel!")
 
 bot.run(os.getenv('TOKEN'))
